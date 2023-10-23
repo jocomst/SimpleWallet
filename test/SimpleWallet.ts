27b0a326b0127e5714302be85e3dcdcef827a3cb
John <johncomstock9716@gmail.com>
2023-10-23 11:12:16 -0400

    trying to fix a lot of issues

--- test/SimpleWallet.ts
+++ test/SimpleWallet.ts
@@ -1,47 +1,58 @@
 import { expect } from "chai";
-import { ethers } from "hardhat";
-import { Signer } from "ethers"; // Import the Signer type
+import "@nomicfoundation/hardhat-toolbox";
+import { Signer, ContractFactory } from "ethers";
+import hre, { ethers } from "hardhat";
 
 describe("SimpleWallet", function () {
-  let SimpleWallet: any; // Replace "any" with the contract type if available
-  let simpleWallet: any; // Replace "any" with the contract instance type
+  let SimpleWallet: ContractFactory;
+  let simpleWallet: any;
   let owner: Signer;
   let user1: Signer;
 
   before(async function () {
-    [owner, user1] = await ethers.getSigners();
-    SimpleWallet = await ethers.getContractFactory("SimpleWallet");
+    [owner, user1] = await hre.ethers.getSigners();
+    SimpleWallet = await hre.ethers.getContractFactory("SimpleWallet");
     simpleWallet = await SimpleWallet.deploy();
   });
 
   describe("Deployment", function () {
     it("Should set the correct owner", async function () {
-      expect(await simpleWallet.owner()).to.equal(owner.address);
+      const ownerAddress = await owner.getAddress();
+      const contractOwner = await simpleWallet.owner();
+      expect(contractOwner).to.equal(ownerAddress);
     });
 
     it("Should have a balance of 0", async function () {
-      const balance = await simpleWallet.getBalance();
+      const balance = await hre.ethers.provider.getBalance(
+        simpleWallet.address
+      );
       expect(balance).to.equal(0);
     });
   });
 
   describe("Deposits and Withdrawals", function () {
     it("Should deposit and withdraw funds correctly", async function () {
-      const depositAmount = ethers.utils.parseEther("1");
+      const depositAmount = ethers.parseEther("1");
       await simpleWallet.deposit({ value: depositAmount });
-      const initialBalance = await simpleWallet.getBalance();
+      const initialBalance = await hre.ethers.provider.getBalance(
+        simpleWallet.address
+      );
       expect(initialBalance).to.equal(depositAmount);
 
-      const withdrawAmount = ethers.utils.parseEther("0.5");
+      const withdrawAmount = ethers.parseEther("0.5");
       await simpleWallet.withdraw(withdrawAmount);
-      const finalBalance = await simpleWallet.getBalance();
-      expect(finalBalance).to.equal(depositAmount.sub(withdrawAmount));
+      const finalBalance = await hre.ethers.provider.getBalance(
+        simpleWallet.address
+      );
+      expect(finalBalance).to.equal(depositAmount - withdrawAmount);
     });
 
     it("Should prevent withdrawals with insufficient balance", async function () {
-      const initialBalance = await simpleWallet.getBalance();
-      const withdrawAmount = ethers.utils.parseEther("2");
-      await expect(simpleWallet.withdraw(withdrawAmount)).to.be.revertedWith(
+      const initialBalance = await hre.ethers.provider.getBalance(
+        simpleWallet.address
+      );
+      const withdrawAmount = ethers.parseEther("2");
+      await expect(simpleWallet.withdraw(withdrawAmount)).to.be.rejectedWith(
         "Insufficient balance"
       );
     });
